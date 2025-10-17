b8c611f76ff07b8f024cee7c396d9f112981e832
Darshani15 <divyadarshanikaundar@gmail.com>
2025-10-18 00:43:13 +0530

    app.py

--- app.py
+++ app.py
@@ -1,13 +1,13 @@
 import streamlit as st
 from PIL import Image, ImageEnhance
 
-st.set_page_config(page_title="🎨 Image Enhancer Studio", page_icon="✨")
+st.set_page_config(page_title="🎨 Pic Perfect : Online Image Processing", page_icon="✨")
 
 st.markdown(
     """
-    <div style="text-align:center; background-color:#D0F4F4; padding:20px; border-radius:10px;">
-        <h1 style="color:#008080;">🎨 Image Enhancer Studio</h1>
-        <p style="font-size:18px; color:#004C4C;">
+    <div style="text-align:center; background-color:#E8F5E9; padding:20px; border-radius:10px;">
+        <h1 style="color:#2E7D32;">🎨 Pic Perfect : Online Image Processing</h1>
+        <p style="font-size:18px; color:#1B5E20;">
             Upload, edit & enhance your images with ease 🌿
         </p>
     </div>
@@ -21,9 +21,9 @@ if uploaded_file:
     image = Image.open(uploaded_file)
     st.image(image, caption="Original Image", use_container_width=True)
 
-    brightness = st.slider("☀️ Brightness", 0.0, 2.0, 1.0)
+    brightness = st.slider("☀ Brightness", 0.0, 2.0, 1.0)
     contrast = st.slider("🌓 Contrast", 0.0, 2.0, 1.0)
-    sharpness = st.slider("✏️ Sharpness", 0.0, 2.0, 1.0)
+    sharpness = st.slider("✏ Sharpness", 0.0, 2.0, 1.0)
     grayscale = st.checkbox("⚪ Convert to Grayscale")
 
     enhancer = ImageEnhance.Brightness(image)
@@ -40,8 +40,8 @@ if uploaded_file:
 
     st.markdown(
         """
-        <div style="text-align:center; background-color:#B2DFDB; padding:10px; border-radius:8px;">
-            <h3 style="color:#006666;">✨ Processed Image ✨</h3>
+        <div style="text-align:center; background-color:#C8E6C9; padding:10px; border-radius:8px;">
+            <h3 style="color:#1B5E20;">↻ ⏳Processed Image 🔜</h3>
         </div>
         """,
         unsafe_allow_html=True
@@ -57,4 +57,3 @@ if uploaded_file:
             file_name="processed.png",
             mime="image/png"
         )
-
